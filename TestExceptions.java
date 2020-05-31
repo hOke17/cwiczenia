932e958201aee7dbe491a9ea57d63355d0db22a5
hOke17 <64348900+hOke17@users.noreply.github.com>
2020-05-31 08:39:54 +0200

    Add files via upload

---
+++ TestExceptions.java
@@ -0,0 +1,13 @@
+public class TestExceptions {
+    public static void main(String[] args) {
+        try {
+        for ( int i = 0; true; i++ ) {
+            System.out.println("args[" + i + "] is '" + args[i] + "'");
+        }
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.err.println("Exception caught: " + e );
+            System.err.println("Quiting...");
+
+    }
+}
+    }
